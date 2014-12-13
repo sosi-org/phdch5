8d56ffda79bb190feaebdf720d2dccc7633fe1f5
Sohail <sohale@gmail.com>
2014-12-13 00:51:02 +0000

    make_signals4.py

--- make_signals_evo.py
+++ make_signals_evo.py
@@ -34,73 +34,90 @@ def make_lor1(ntr,nlen):
     b = numpy.cumsum(a, axis=1 )
 
 
-
+def verbose(s):
+    print(s)
 #np.array([1, 2, 3], dtype=complex)
 
 def erp1 ( n , tau_sec , fs_Hz ):
     # @type n: int
     # @type tau_sec: float
     # @type fs_Hz: float
+    tau_sec=float(tau_sec)
+    fs_Hz=float(fs_Hz)
     
     # the kernel function :
-    klen = 5 * tau_sec / fs_Hz
-    print klen #0.0005
+    #klen = 5 * tau_sec / fs_Hz
+    klen = 5 * tau_sec * fs_Hz
     
+    assert tau_sec>0 #don't include 0
+    
+    klen=int(numpy.ceil(klen))
+    #or insted of ceil, do this:
+    assert klen>0
     #not sure
-    if klen<2:
-        klen=2
-    
+    if klen<1:
+        verbose("Warning: kernel len is small: %d. making it=1"%klen)
+        klen=1
+
+    #assert klen<n*100
+
+    verbose(klen)
+    verbose(n)
+    verbose(n*klen)
+    assert klen<n*1000
+    assert n>0
+    assert n*klen<1E7 #takes up to a few seconds. todo: improve these conditions.
+    #assert n*klen<1E8
+    #klen is ready
+
+        
     #t = [ 0 : ( klen -1) ] / fs_Hz ;
-    t = np.linspace(0 , klen -1, klen).T / fs_Hz 
-    #print(t[:10])
-
-    t0=t
-    t=t.reshape(( t.shape[0],1 )) #single column
-
-    #print("t=%r"%t)
-    #print("t=%r"%t.transpose())
-    #print "***********************"
-    ##print (1,t.shape[0] )
-    ##t=t.reshape((t.shape[0],1 )) #single column
-    #print "--------"
-    #print t.shape
-    #print t.transpose().shape
-    ##print t.shape
-    # SIZE PROBLEM:  the shape [1,5] cannot be convolved. you need to change the shape: [n,] (RESOLVED)
+    t = np.linspace(0 , klen -1, klen) / fs_Hz
+    k = np.exp ( -t / tau_sec )
+    x = np.random.randn (n + klen *2 + 4 )     # the white noise :
+    #lim = [ klen +3 , n + klen +2]
+    lim = [ klen +3 , n + klen +3] #the size is different to matlab
     
-    k = np.exp ( -t0 / tau_sec ) ;
-    lim = [ klen +3 , n + klen +2] ;
-    # the white noise :
-    print "***"
-    x = np.random.randn (n + klen *2 + 4 ) ;
-    print "***"
-    k = k.transpose()
-    # the convolution ( integration ):
-    #r = np.conv ( x , k / fs_Hz ) * fs_Hz ; #full, same, or valid?
-    #print("x=%r"%x)
-    #print("k=%r"% (k / fs_Hz) )
-    print x.shape
-    print k.shape
-    #print("x: %r"% (x.shape) )
-    #print("k: %r"% (k.shape) )
-    #r = np.convolve ( x , k / fs_Hz ) * fs_Hz ;
-    r = np.convolve ( x , k / fs_Hz ) * fs_Hz ;
-    
-    
-    
-    
-    
+    #print x.shape #114
+    #print k.shape #5
+    #print t.shape #5
+    #print "==========="
+    #print n
+    # 'full' is the default mode. 
+    r = np.convolve ( x , k / fs_Hz , 'full') * fs_Hz 
     # fixing the phase - shift :
-    r = r [ lim [0] : lim [1] ] ;
+    r = r [ lim [0] : lim [1] ]
     return r
 
+
+def timesarr(n,fs_Hz):
+    return np.linspace(0 , n-1, n) / fs_Hz
+    # x = np.arange(0, 5, 0.1) #also try this
+    
+    
+    
 #main
 ntr=100
 nlen=100000
 #make_lor1(ntr,nlen)
 
 
-z=erp1(10,0.1,1000)
+#z=erp1(10,0.1,fs_Hz)
+#z=erp1(100,0.001,fs_Hz)
+#z=erp1(100,1000,fs_Hz)
+fs_Hz=1000
+l=1000
+#z=erp1(l,10,fs_Hz) #10 sec is very slow.
+z=erp1(l,10,fs_Hz) 
+#test: check size, check small lengths, check 0, 0.0001, ...  n=0, n=1
+ts=timesarr(l,fs_Hz)
+import matplotlib.pyplot
+print "---------"
+print z.shape
+print ts.shape
+p1=matplotlib.pyplot.plot(ts,z)
+matplotlib.pyplot.show()
+
 
 
 
